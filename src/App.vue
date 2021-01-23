99d4b50595c32681f40d07354be0a34bfd7e59b9
Vitor <jotavitor2018@gmail.com>
2021-01-23 20:32:22 -0300

    atualizacao bulma

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+            <img src="./assets/pokelogo.png">
+            <hr>
+            <h4 class="is-size-4">Pokedex</h4>
+            <input type="text" name="" id="" placeholder="Gotta catch em' all!" v-model="search" class="input is-rounded">
+            <hr>
+              <button class="button is-success">Search!</button>
+
+      <div v-for="(poke,index) in SearchResult" :key="index">
+        <Pokemon :name="poke.name" :url="poke.url" :num="index+1"/> 
+      </div>      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
+import Pokemon from './components/Pokemon';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      pokemons: [],
+      search: ''
+    }
+  },
+    created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      console.log("Gotcha");
+      this.pokemons = res.data.results;
+    })
+  },
+  components:{
+    Pokemon
+  },
+  computed: {
+    SearchResult: function(){
+      if(this.search == '' || this.search == ' '){
+        return this.pokemons;
+      }else{
+        return this.pokemons.filter(pokemon => pokemon.name == this.search);
+      
+    }
   }
 }
+}  
 </script>
 
 <style>
